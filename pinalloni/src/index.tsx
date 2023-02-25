1574f415bcd4ab8fcffac3b3c3a8bd10f249a8c2
pinalli <betopinali@gmail.com>
2023-02-25 15:22:04 -0300

    build svg

--- pinalloni/src/index.tsx
+++ pinalloni/src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Menu from './pages/menu';
 
 ReactDOM.render(
     <React.StrictMode>
-    <div>Pinalloni</div>
-  </React.StrictMode>,
+      <Menu/>
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
---
+++ pinalloni/src/pages/menu/index.tsx
@@ -0,0 +1,13 @@
+import styles from './Menu.module.scss';
+import {ReactComponent as Logo }from '../../assets/logo.svg';
+
+export default function Menu() {
+    return (
+        <main>
+            {/* Menu.module.scss */}
+       <nav className={styles.menu}> 
+        <Logo/>
+       </nav>
+        </main>
+    );
+}
